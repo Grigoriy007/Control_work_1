eb22c64a637c9572b0ef71e4fae702c60168bbe5
Grigoriy Legal <grigoriylegal@yandex.ru>
2022-11-29 19:51:34 +0300

    Создал метод для формирования массива из 3 символов (определение его длинны)

--- Solution/Program.cs
+++ Solution/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] FindLength(string[] find1)
+{
+
+    int count = default;
+    for (int i = 0; i < find1.Length; i++)
+    {
+        if (find1[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+
+    string[] find2 = new string[count];
+
+    return find2;
+}
